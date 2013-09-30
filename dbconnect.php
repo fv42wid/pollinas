ceb368093e99c5d240fc738cf8511c5dcb8cfd37
Frank Vinci <fv42wid@yahoo.com>
2013-09-29 21:41:48 -0400

    updated dbconnect again

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("www.vtwebconsulting.com", "vtwebcon_weekend", "imsMay33", "vtwebcon_pollinas");
+$con = mysqli_connect("50.31.162.18", "vtwebcon_weekend", "imsMay33", "vtwebcon_pollinas");
 	
 $execute = mysqli_query($con, $query);
 mysqli_close($con);
